62f424c8c55b5bb2361666b1b3cb632eb4d78844
Lenrie13 <lenrie.ogutu13@gmail.com>
2024-07-09 21:42:48 +0300

    update objects lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+
+let employee = {
+    name: "Sam",
+    streetAddress: "23rd Trinity Avenue"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee, streetAddress: "11 Broadway"
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  };
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  };
